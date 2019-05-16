5f91f101b43094d6254e09d210830870eba40365
mattl <mattl@google.com>
2019-05-16 16:13:04 -0400

    s/activites/activities/
    PiperOrigin-RevId: 248585875

--- glazier/lib/actions/sysprep.py
+++ glazier/lib/actions/sysprep.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Actions for sysprep-related activites."""
+"""Actions for sysprep-related activities."""
 
 import logging
 import re
