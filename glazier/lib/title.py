8e2138ad6d121645922456875aec7dc86a73b208
Dan Tsekhanskiy <dantsek@google.com>
2020-02-12 06:38:38 -0800

    Fix top-level comment for title/title_test.
    PiperOrigin-RevId: 294655279

--- glazier/lib/title.py
+++ glazier/lib/title.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Encapsulates information pertaining to WinPE during the image."""
+"""Construct and set the current console title."""
 
 from __future__ import absolute_import
 from __future__ import division
