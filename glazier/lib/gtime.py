1f6df5b484e598efcb17ec0250c79e2b3b958a8e
Matt LaPlante <mattl@google.com>
2020-05-11 13:29:55 -0700

    Add a glazier time helper to standardize time related operations.
    PiperOrigin-RevId: 310981121

---
+++ glazier/lib/gtime.py
@@ -0,0 +1,31 @@
+# Lint as: python3
+# Copyright 2019 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Time related helper functions."""
+
+import datetime
+
+from absl import flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer('default_tz_offset', 0,
+                     'Default timezone offset used for time calculations.')
+
+
+def now(offset: int = None) -> datetime.datetime:
+  if not offset:
+    offset = FLAGS.default_tz_offset
+  tz = datetime.timezone(datetime.timedelta(offset))
+  return datetime.datetime.now(tz)
