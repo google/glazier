a00964a46d35de3f6193f0d3f1b4e490e9630e19
Matt LaPlante <mattl@google.com>
2021-05-17 13:45:39 -0700

    Open source the identity library.
    PiperOrigin-RevId: 374271792

---
+++ go/identity/identity.go
@@ -0,0 +1,49 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package identity provides helpers for managing host identity.
+package identity
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/StackExchange/wmi"
+)
+
+type Win32_ComputerSystem struct {
+	Partofdomain bool
+}
+
+// DomainJoined attempts to determine whether the machine is actively domain joined.
+func DomainJoined() (bool, error) {
+	var c []Win32_ComputerSystem
+	q := wmi.CreateQuery(&c, "")
+
+	if err := wmi.Query(q, &c); err != nil {
+		return false, err
+	}
+	if len(c) < 1 {
+		return false, errors.New("no result from wmi query")
+	}
+	return c[0].Partofdomain, nil
+}
+
+// ImageID generates a Glazier unique Image ID.
+// Serial should be the device serial number, as reported by the BIOS.
+func ImageID(serial string) string {
+	u := strings.ToUpper(uuid.NewString())
+	return strings.ToUpper(serial) + "-" + u[len(u)-7:]
+}
