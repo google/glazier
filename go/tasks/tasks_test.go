1baf92d5027699d0c3902e6f3d9afc6be0b711b4
Matt LaPlante <mattl@google.com>
2021-05-18 12:14:53 -0700

    Fix test table loop.
    PiperOrigin-RevId: 374475551

--- go/tasks/tasks_test.go
+++ go/tasks/tasks_test.go
@@ -47,6 +47,7 @@ func TestTaskExists(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.desc, func(t *testing.T) {
 			t.Parallel()
 			got := taskMatcher(tt.in, tt.coll)
