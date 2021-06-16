b5cded3403c2a07807eac4a0232a6046cb3bf5fa
Matt LaPlante <mattl@google.com>
2021-06-16 06:42:30 -0700

    Refactor the Bitlocker package for greater flexibility.
    * Create a Volume that can hold an ongoing connection to a volume to be created.
    * Move each individual operation to a separate function.
    * Add ProtectKeyWithPassphrase support.
    * Add ProtectKeyWithNumericalPassword support.
    * Add PrepareVolume.
    * Add a few custom types for validating arguments.
    
    PiperOrigin-RevId: 379708267

--- go/bitlocker/bitlocker.go
+++ go/bitlocker/bitlocker.go
@@ -18,6 +18,7 @@ package bitlocker
 import (
 	"fmt"
 
+	"github.com/scjalliance/comshim"
 	"github.com/google/logger"
 	"github.com/go-ole/go-ole"
 	"github.com/go-ole/go-ole/oleutil"
@@ -48,38 +49,12 @@ func BackupToAD() error {
 	return funcBackup(volIDs)
 }
 
-type wmi struct {
-	intf *ole.IDispatch
-	svc  *ole.IDispatch
-}
-
-func (w *wmi) connect() error {
-	unknown, err := oleutil.CreateObject("WbemScripting.SWbemLocator")
-	if err != nil {
-		return fmt.Errorf("unable to create initial object, %w", err)
-	}
-	defer unknown.Release()
-	w.intf, err = unknown.QueryInterface(ole.IID_IDispatch)
-	if err != nil {
-		return fmt.Errorf("unable to create initial object, %w", err)
-	}
-	serviceRaw, err := oleutil.CallMethod(w.intf, "ConnectServer", nil, `\\.\ROOT\CIMV2\Security\MicrosoftVolumeEncryption`)
-	if err != nil {
-		return fmt.Errorf("permission denied: %w", err)
-	}
-	w.svc = serviceRaw.ToIDispatch()
-	return nil
-}
-
-func (w *wmi) close() {
-	w.svc.Release()
-	w.intf.Release()
-}
+// Encryption Methods
+// https://docs.microsoft.com/en-us/windows/win32/secprov/getencryptionmethod-win32-encryptablevolume
+type EncryptionMethod int32
 
 const (
-	// Encryption Methods
-	// https://docs.microsoft.com/en-us/windows/win32/secprov/getencryptionmethod-win32-encryptablevolume
-	None int32 = iota
+	None EncryptionMethod = iota
 	AES128WithDiffuser
 	AES256WithDiffuser
 	AES128
@@ -87,23 +62,45 @@ const (
 	HardwareEncryption
 	XtsAES128
 	XtsAES256
+)
 
-	// Encryption Flags
-	// https://docs.microsoft.com/en-us/windows/win32/secprov/encrypt-win32-encryptablevolume
-	EncryptDataOnly    int32 = 0x00000001
-	EncryptDemandWipe  int32 = 0x00000002
-	EncryptSynchronous int32 = 0x00010000
+// Encryption Flags
+// https://docs.microsoft.com/en-us/windows/win32/secprov/encrypt-win32-encryptablevolume
+type EncryptionFlag int32
+
+const (
+	EncryptDataOnly    EncryptionFlag = 0x00000001
+	EncryptDemandWipe  EncryptionFlag = 0x00000002
+	EncryptSynchronous EncryptionFlag = 0x00010000
 
 	// Error Codes
-	ERROR_IO_DEVICE        int32 = -2147023779
-	FVE_E_BOOTABLE_CDDVD   int32 = -2144272336
-	FVE_E_PROTECTOR_EXISTS int32 = -2144272335
+	ERROR_IO_DEVICE                     int32 = -2147023779
+	FVE_E_EDRIVE_INCOMPATIBLE_VOLUME    int32 = -2144272206
+	FVE_E_NO_TPM_WITH_PASSPHRASE        int32 = -2144272212
+	FVE_E_PASSPHRASE_TOO_LONG           int32 = -2144272214
+	FVE_E_POLICY_PASSPHRASE_NOT_ALLOWED int32 = -2144272278
+	FVE_E_NOT_DECRYPTED                 int32 = -2144272327
+	FVE_E_INVALID_PASSWORD_FORMAT       int32 = -2144272331
+	FVE_E_BOOTABLE_CDDVD                int32 = -2144272336
+	FVE_E_PROTECTOR_EXISTS              int32 = -2144272335
 )
 
 func encryptErrHandler(val int32) error {
 	switch val {
 	case ERROR_IO_DEVICE:
 		return fmt.Errorf("an I/O error has occurred during encryption; the device may need to be reset")
+	case FVE_E_EDRIVE_INCOMPATIBLE_VOLUME:
+		return fmt.Errorf("the drive specified does not support hardware-based encryption")
+	case FVE_E_NO_TPM_WITH_PASSPHRASE:
+		return fmt.Errorf("a TPM key protector cannot be added because a password protector exists on the drive")
+	case FVE_E_PASSPHRASE_TOO_LONG:
+		return fmt.Errorf("the passphrase cannot exceed 256 characters")
+	case FVE_E_POLICY_PASSPHRASE_NOT_ALLOWED:
+		return fmt.Errorf("Group Policy settings do not permit the creation of a password")
+	case FVE_E_NOT_DECRYPTED:
+		return fmt.Errorf("the drive must be fully decrypted to complete this operation")
+	case FVE_E_INVALID_PASSWORD_FORMAT:
+		return fmt.Errorf("the format of the recovery password provided is invalid")
 	case FVE_E_BOOTABLE_CDDVD:
 		return fmt.Errorf("BitLocker Drive Encryption detected bootable media (CD or DVD) in the computer. " +
 			"Remove the media and restart the computer before configuring BitLocker.")
@@ -114,47 +111,179 @@ func encryptErrHandler(val int32) error {
 	}
 }
 
-// EncryptWithTPM encrypts the drive with Bitlocker using TPM key protection.
+// A Volume tracks an open encryptable volume.
+type Volume struct {
+	letter  string
+	handle  *ole.IDispatch
+	wmiIntf *ole.IDispatch
+	wmiSvc  *ole.IDispatch
+}
+
+// Close frees all resources associated with a volume.
+func (v *Volume) Close() {
+	v.handle.Release()
+	v.wmiIntf.Release()
+	v.wmiSvc.Release()
+	comshim.Done()
+}
+
+// Connect connects to an encryptable volume in order to manage it.
+// You must call Close() to release the volume when finished.
 //
-// Example: bitlocker.EncryptWithTPM("c:", bitlocker.XtsAES256, bitlocker.EncryptDataOnly)
-func EncryptWithTPM(driveLetter string, method int32, flags int32) error {
-	ole.CoInitialize(0)
-	defer ole.CoUninitialize()
-	w := &wmi{}
-	if err := w.connect(); err != nil {
-		return fmt.Errorf("wmi.Connect: %w", err)
-	}
-	defer w.close()
-	raw, err := oleutil.CallMethod(w.svc, "ExecQuery",
-		"SELECT * FROM Win32_EncryptableVolume WHERE DriveLetter = '"+driveLetter+"'")
+// Example: bitlocker.Connect("c:")
+func Connect(driveLetter string) (Volume, error) {
+	comshim.Add(1)
+	v := Volume{letter: driveLetter}
+
+	unknown, err := oleutil.CreateObject("WbemScripting.SWbemLocator")
 	if err != nil {
-		return fmt.Errorf("ExecQuery: %w", err)
+		comshim.Done()
+		return v, fmt.Errorf("CreateObject: %w", err)
+	}
+	defer unknown.Release()
+	v.wmiIntf, err = unknown.QueryInterface(ole.IID_IDispatch)
+	if err != nil {
+		comshim.Done()
+		return v, fmt.Errorf("QueryInterface: %w", err)
+	}
+	serviceRaw, err := oleutil.CallMethod(v.wmiIntf, "ConnectServer", nil, `\\.\ROOT\CIMV2\Security\MicrosoftVolumeEncryption`)
+	if err != nil {
+		v.Close()
+		return v, fmt.Errorf("ConnectServer: %w", err)
+	}
+	v.wmiSvc = serviceRaw.ToIDispatch()
+
+	raw, err := oleutil.CallMethod(v.wmiSvc, "ExecQuery", "SELECT * FROM Win32_EncryptableVolume WHERE DriveLetter = '"+driveLetter+"'")
+	if err != nil {
+		v.Close()
+		return v, fmt.Errorf("ExecQuery: %w", err)
 	}
 	result := raw.ToIDispatch()
 	defer result.Release()
 
 	itemRaw, err := oleutil.CallMethod(result, "ItemIndex", 0)
 	if err != nil {
-		return fmt.Errorf("failed to fetch result row while processing BitLocker info: %w", err)
+		v.Close()
+		return v, fmt.Errorf("failed to fetch result row while processing BitLocker info: %w", err)
 	}
-	item := itemRaw.ToIDispatch()
-	defer item.Release()
+	v.handle = itemRaw.ToIDispatch()
 
-	// https://docs.microsoft.com/en-us/windows/win32/secprov/protectkeywithtpm-win32-encryptablevolume
+	return v, nil
+}
+
+// Encrypt encrypts the volume.
+//
+// Example: vol.Encrypt(bitlocker.XtsAES256, bitlocker.EncryptDataOnly)
+//
+// Ref: https://docs.microsoft.com/en-us/windows/win32/secprov/protectkeywithtpm-win32-encryptablevolume
+func (v *Volume) Encrypt(method EncryptionMethod, flags EncryptionFlag) error {
+	resultRaw, err := oleutil.CallMethod(v.handle, "Encrypt", int32(method), int32(flags))
+	if err != nil {
+		return fmt.Errorf("Encrypt(%s): %w", v.letter, err)
+	} else if val, ok := resultRaw.Value().(int32); val != 0 || !ok {
+		return fmt.Errorf("Encrypt(%s): %w", v.letter, encryptErrHandler(val))
+	}
+
+	return nil
+}
+
+// DiscoveryVolumeType specifies the type of discovery volume to be used by Prepare.
+//
+// Ref: https://docs.microsoft.com/en-us/windows/win32/secprov/preparevolume-win32-encryptablevolume
+type DiscoveryVolumeType string
+
+const (
+	// VolumeTypeNone indicates no discovery volume. This value creates a native BitLocker volume.
+	VolumeTypeNone DiscoveryVolumeType = "<none>"
+	// VolumeTypeDefault indicates the default behavior.
+	VolumeTypeDefault DiscoveryVolumeType = "<default>"
+	// VolumeTypeFAT32 creates a FAT32 discovery volume.
+	VolumeTypeFAT32 DiscoveryVolumeType = "FAT32"
+)
+
+// ForceEncryptionType specifies the encryption type to be used when calling Prepare on the volume.
+//
+// Ref: https://docs.microsoft.com/en-us/windows/win32/secprov/preparevolume-win32-encryptablevolume
+type ForceEncryptionType int32
+
+const (
+	// EncryptionTypeUnspecified indicates that the encryption type is not specified.
+	EncryptionTypeUnspecified ForceEncryptionType = 0
+	// EncryptionTypeSoftware specifies software encryption.
+	EncryptionTypeSoftware ForceEncryptionType = 1
+	// EncryptionTypeHardware specifies hardware encryption.
+	EncryptionTypeHardware ForceEncryptionType = 2
+)
+
+// Prepare prepares a new Bitlocker Volume. This should be called BEFORE any key protectors are added.
+//
+// Example: vol.Prepare(bitlocker.VolumeTypeDefault, bitlocker.EncryptionTypeHardware)
+//
+// Ref: https://docs.microsoft.com/en-us/windows/win32/secprov/preparevolume-win32-encryptablevolume
+func (v *Volume) Prepare(volType DiscoveryVolumeType, encType ForceEncryptionType) error {
+	resultRaw, err := oleutil.CallMethod(v.handle, "PrepareVolume", string(volType), int32(encType))
+	if err != nil {
+		return fmt.Errorf("PrepareVolume(%s): %w", v.letter, err)
+	} else if val, ok := resultRaw.Value().(int32); val != 0 || !ok {
+		return fmt.Errorf("PrepareVolume(%s): %w", v.letter, encryptErrHandler(val))
+	}
+	return nil
+}
+
+// ProtectWithNumericalPassword adds a numerical password key protector.
+//
+// Leave password as a blank string to have one auto-generated by Windows. (Recommended)
+//
+// In Powershell this is referred to as a RecoveryPasswordProtector.
+//
+// Ref: https://docs.microsoft.com/en-us/windows/win32/secprov/protectkeywithnumericalpassword-win32-encryptablevolume
+func (v *Volume) ProtectWithNumericalPassword(password string) error {
 	var volumeKeyProtectorID ole.VARIANT
 	ole.VariantInit(&volumeKeyProtectorID)
-	resultRaw, err := oleutil.CallMethod(item, "ProtectKeyWithTPM", nil, nil, &volumeKeyProtectorID)
+	var resultRaw *ole.VARIANT
+	var err error
+	if password != "" {
+		resultRaw, err = oleutil.CallMethod(v.handle, "ProtectWithNumericalPassword", nil, password, &volumeKeyProtectorID)
+	} else {
+		resultRaw, err = oleutil.CallMethod(v.handle, "ProtectWithNumericalPassword", nil, nil, &volumeKeyProtectorID)
+	}
 	if err != nil {
-		return fmt.Errorf("ProtectKeyWithTPM(%s): %w", driveLetter, err)
+		return fmt.Errorf("ProtectWithNumericalPassword(%s): %w", v.letter, err)
 	} else if val, ok := resultRaw.Value().(int32); val != 0 || !ok {
-		return fmt.Errorf("ProtectKeyWithTPM(%s): %w", driveLetter, encryptErrHandler(val))
+		return fmt.Errorf("ProtectWithNumericalPassword(%s): %w", v.letter, encryptErrHandler(val))
 	}
 
-	resultRaw, err = oleutil.CallMethod(item, "Encrypt", method, flags)
+	return nil
+}
+
+// ProtectWithPassphrase adds a passphrase key protector.
+//
+// Ref: https://docs.microsoft.com/en-us/windows/win32/secprov/protectkeywithpassphrase-win32-encryptablevolume
+func (v *Volume) ProtectWithPassphrase(passphrase string) error {
+	var volumeKeyProtectorID ole.VARIANT
+	ole.VariantInit(&volumeKeyProtectorID)
+	resultRaw, err := oleutil.CallMethod(v.handle, "ProtectKeyWithPassphrase", nil, passphrase, &volumeKeyProtectorID)
 	if err != nil {
-		return fmt.Errorf("Encrypt(%s): %w", driveLetter, err)
+		return fmt.Errorf("ProtectWithPassphrase(%s): %w", v.letter, err)
 	} else if val, ok := resultRaw.Value().(int32); val != 0 || !ok {
-		return fmt.Errorf("Encrypt(%s): %w", driveLetter, encryptErrHandler(val))
+		return fmt.Errorf("ProtectWithPassphrase(%s): %w", v.letter, encryptErrHandler(val))
 	}
+
+	return nil
+}
+
+// ProtectWithTPM adds the TPM key protector.
+//
+// Ref: https://docs.microsoft.com/en-us/windows/win32/secprov/protectkeywithtpm-win32-encryptablevolume
+func (v *Volume) ProtectWithTPM() error {
+	var volumeKeyProtectorID ole.VARIANT
+	ole.VariantInit(&volumeKeyProtectorID)
+	resultRaw, err := oleutil.CallMethod(v.handle, "ProtectKeyWithTPM", nil, nil, &volumeKeyProtectorID)
+	if err != nil {
+		return fmt.Errorf("ProtectKeyWithTPM(%s): %w", v.letter, err)
+	} else if val, ok := resultRaw.Value().(int32); val != 0 || !ok {
+		return fmt.Errorf("ProtectKeyWithTPM(%s): %w", v.letter, encryptErrHandler(val))
+	}
+
 	return nil
 }
