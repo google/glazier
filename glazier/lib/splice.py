929817e9bd1f67dd9eb3f16f0df8a6c073bd35c6
Christopher Gearnsc <gearnsc@google.com>
2021-08-16 17:03:29 -0700

    default to unattended joins
    PiperOrigin-RevId: 391170374

--- glazier/lib/splice.py
+++ glazier/lib/splice.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Levarage Splice for offline domain join."""
 
 import logging
@@ -70,21 +69,35 @@ class Splice(object):
 
     return fr'{constants.DOMAIN_NAME}\{username}'
 
-  def domain_join(self, max_retries: Optional[int] = 5):
+  def domain_join(self,
+                  max_retries: Optional[int] = 5,
+                  unattended: Optional[bool] = True,
+                  fallback: Optional[bool] = True):
     """Execute the Splice CLI with defined flags.
 
     Args:
       max_retries: The number of times to attempt to download a file if the
-      first attempt fails. A negative number implies infinite.
+        first attempt fails. A negative number implies infinite.
+      unattended: If true, execute splice in unattended mode and do not prompt
+        for user credentials.
+      fallback: If true and an unattended join fails, reattempt and request user
+        credentials.
 
     Raises:
       Error: Domain join failed.
     """
     args = [
-        '-cert_issuer=client', f'-cert_container={self.cert_container}',
-        f'-name={self._get_hostname()}', f'-server={self.splice_server}',
-        '-really_join=true', f'-user_name={self._get_username()}'
+        '-cert_issuer=client',
+        f'-cert_container={self.cert_container}',
+        f'-name={self._get_hostname()}',
+        f'-server={self.splice_server}',
+        '-really_join=true',
     ]
+
+    if unattended:
+      args += ['-unattended=true']
+    else:
+      args += [f'-user_name={self._get_username()}']
 
     attempt = 0
     sleep = 30
@@ -99,9 +112,12 @@ class Splice(object):
               'Domain join attempt %d of %d failed. Retrying in %d second(s).',
               attempt, max_retries, sleep)
           time.sleep(sleep)
+        elif unattended and fallback:
+          logging.warning(
+              'Unattended domain join failed, retrying with user credentials.')
+          self.domain_join(max_retries, False, False)
         else:
           raise Error(f'Failed to join domain after {max_retries} attempt(s).')
       else:
-        logging.info('Domain join succeeded after %d attempt(s).',
-                     attempt)
+        logging.info('Domain join succeeded after %d attempt(s).', attempt)
         break
