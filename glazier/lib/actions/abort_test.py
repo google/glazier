a360dbaab48e19c77e9f8126db549b059265ea5d
Matt Doyle <chief@google.com>
2022-07-26 07:46:26 -0700

    Glazier test parameterization.
    PiperOrigin-RevId: 463334177

--- glazier/lib/actions/abort_test.py
+++ glazier/lib/actions/abort_test.py
@@ -11,52 +11,68 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Tests for glazier.lib.actions.abort."""
 
 from unittest import mock
 
 from absl.testing import absltest
+from absl.testing import parameterized
+
 from glazier.lib.actions import abort
 
 
-class AbortTest(absltest.TestCase):
+class AbortTest(parameterized.TestCase):
 
   @mock.patch('glazier.lib.buildinfo.BuildInfo', autospec=True)
   def test_abort(self, mock_buildinfo):
     ab = abort.Abort(['abort message'], mock_buildinfo)
     self.assertRaises(abort.ActionError, ab.Run)
 
-  # TODO(b/237812617): Parameterize this test.
-  def test_abort_validate(self):
-    ab = abort.Abort('abort message', None)
-    self.assertRaises(abort.ValidationError, ab.Validate)
-    ab = abort.Abort([1, 2, 3], None)
-    self.assertRaises(abort.ValidationError, ab.Validate)
-    ab = abort.Abort([1], None)
-    self.assertRaises(abort.ValidationError, ab.Validate)
+  @parameterized.named_parameters(
+      ('_invalid_argument_type_str', 'abort message', None),
+      ('_invalid_args_length', [1, 2, 3], None),
+      ('_invalid_argument_type_int', [1], None),
+  )
+  def test_abort_validation_error(self, action_args, build_info):
+    ab = abort.Abort(action_args, build_info)
+    with self.assertRaises(abort.ValidationError):
+      ab.Validate()
+
+  def test_abort_validation_success(self):
     ab = abort.Abort(['Error Message'], None)
     ab.Validate()
 
+  @parameterized.named_parameters(
+      ('_none', None),
+      ('_invalid_response', 'no thanks'),
+  )
   @mock.patch('glazier.lib.buildinfo.BuildInfo', autospec=True)
   @mock.patch('glazier.lib.interact.Prompt', autospec=True)
-  def test_warn(self, mock_prompt, mock_buildinfo):
+  def test_warn_action_error(self, prompt_return_value, mock_prompt,
+                             mock_buildinfo):
     warn = abort.Warn(['warning message'], mock_buildinfo)
-    mock_prompt.return_value = None
-    self.assertRaises(abort.ActionError, warn.Run)
-    mock_prompt.return_value = 'no thanks'
-    self.assertRaises(abort.ActionError, warn.Run)
+    mock_prompt.return_value = prompt_return_value
+    with self.assertRaises(abort.ActionError):
+      warn.Run()
+
+  @mock.patch('glazier.lib.buildinfo.BuildInfo', autospec=True)
+  @mock.patch('glazier.lib.interact.Prompt', autospec=True)
+  def test_warn_action_success(self, mock_prompt, mock_buildinfo):
+    warn = abort.Warn(['warning message'], mock_buildinfo)
     mock_prompt.return_value = 'Y'
     warn.Run()
 
-  # TODO(b/237812617): Parameterize this test.
-  def test_warn_validate(self):
-    warn = abort.Warn('abort message', None)
-    self.assertRaises(abort.ValidationError, warn.Validate)
-    warn = abort.Warn([1, 2, 3], None)
-    self.assertRaises(abort.ValidationError, warn.Validate)
-    warn = abort.Warn([1], None)
-    self.assertRaises(abort.ValidationError, warn.Validate)
+  @parameterized.named_parameters(
+      ('_invalid_argument_type_str', 'abort message', None),
+      ('_invalid_args_length', [1, 2, 3], None),
+      ('_invalid_argument_type_int', [1], None),
+  )
+  def test_warn_validation_error(self, action_args, build_info):
+    warn = abort.Warn(action_args, build_info)
+    with self.assertRaises(abort.ValidationError):
+      warn.Validate()
+
+  def test_warn_validation_success(self):
     warn = abort.Warn(['Error Message'], None)
     warn.Validate()
 
