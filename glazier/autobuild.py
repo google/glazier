e0e2f14170bd5abbad7813485f1503b14fa416e7
John-Michael Mulesa <jmulesa@google.com>
2023-12-04 12:17:46 -0800

    Add battery status check and clock syncing.
    PiperOrigin-RevId: 587803054

--- glazier/autobuild.py
+++ glazier/autobuild.py
@@ -15,13 +15,16 @@
 """Windows installation and configuration tool."""
 
 import os
+import re
 import sys
 
 # do not remove: internal placeholder 1
 
 from absl import app
 from glazier.lib import flags
+from glazier.lib import interact
 from glazier.lib import logs
+from glazier.lib import ntp
 from glazier.lib import title
 from glazier.lib import winpe
 from glazier.lib.config import builder
@@ -29,10 +32,79 @@ from glazier.lib.config import runner
 
 from glazier.lib import buildinfo
 from glazier.lib import constants
+from glazier.lib import errors
 from glazier.lib import terminator
 
 
 logging = logs.logging
+
+
+class Error(errors.GlazierError):
+  pass
+
+
+class BatteryStatusError(Error):
+  """Error class for starting the imaging process while on battery."""
+
+  def __init__(self):
+    super().__init__(
+        error_code=errors.ErrorCode.BATTERY_STATUS_ERROR,
+        message=StripMargin('Imaging process started while on battery'),
+    )
+
+
+def StripMargin(message, deliminator='|'):
+  """Function to strip the margins from multiline strings.
+
+  Equivalent to the stripMargin function in scala:
+  https://www.oreilly.com/library/view/scala-cookbook/9781449340292/ch01s03.html
+
+  Args:
+    message: message to strip margins from
+    deliminator: character to mark the start of a margin (default pipe '|')
+
+  Returns:
+    stripped string
+  """
+  return re.sub(
+      pattern=rf'^[ \t]+{re.escape(deliminator)}',
+      repl='',
+      string=message,
+      flags=re.MULTILINE,
+  )
+
+
+def _SyncClock():
+  """Sync BIOS clock to NTP, to avoid cert/date issues."""
+  try:
+    ntp.SyncClockToNtp(server=flags.NTP_SERVER.value)
+  except ntp.NoNtpResponseError as e:
+    logging.error('NTP synchronization failed ({}).'.format(str(e)))
+
+
+def _CheckBattery(build_info):
+  """Issue a warning if we are starting the imaging process on battery."""
+  if not build_info.IsLaptop():
+    return
+
+  if build_info.IsOnBattery():
+    timeout = 15
+    message = f"""
+    |                       !!!!! Warning !!!!!
+    |
+    |You are attempting to start the imaging process while on battery. It is
+    |highly recommended that you plug your laptop in before continuing.
+    |
+    |This process will exit in {timeout} seconds automatically unless if you
+    |choose to continue.
+    |
+    |  Continue anyways (y/n)?
+    |"""
+    result = interact.Keystroke(
+        message=StripMargin(message), validator=r'^[yYnN].*', timeout=timeout
+    )
+    if not result or result.lower().startswith('n'):
+      raise BatteryStatusError()
 
 
 class AutoBuild(object):
@@ -60,6 +132,9 @@ class AutoBuild(object):
     """Perform the build."""
     try:
       title.set_title()
+      if winpe.check_winpe():
+        _SyncClock()
+        _CheckBattery(self._build_info)
       self._build_info.ImageID()
       self._build_info.BeyondCorp()
 
