a5e7c6ee0aba993a64c2e26d551973444c770ecd
Matthew Oliver <mjo@google.com>
2024-10-03 10:02:45 -0700

    Internal changes
    PiperOrigin-RevId: 681924366

--- go/device/device.go
+++ go/device/device.go
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build windows
-// +build windows
-
 // Package device supports querying information about the local device.
 package device
 
