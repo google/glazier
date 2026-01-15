11a0bc64a1a5f57182c38fac7f3c3ab0314e589d
Matthew Oliver <mjo@google.com>
2026-01-15 13:58:11 -0800

    Add a Go package for Windows domain joining.
    PiperOrigin-RevId: 856806993

---
+++ go/domain/join.go
@@ -0,0 +1,109 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build generate || windows
+// +build generate windows
+
+// Package join provides functions for domain joining a Windows client to a domain.
+package join
+
+import (
+	"fmt"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+const (
+	// Known error codes
+	// nErrSuccess is the value returned on a successful run of NetJoinDomain.
+	// https://docs.microsoft.com/en-us/windows/win32/api/lmjoin/nf-lmjoin-netjoindomain#return-value
+	nErrSuccess = 0
+
+	// Domain Join constants
+	// https://learn.microsoft.com/en-us/windows/win32/api/lmjoin/nf-lmjoin-netjoindomain#parameters
+	server = 0 // use the local machine name
+)
+
+// DomainJoinOptions specifies options for the domain join operation.
+// See https://learn.microsoft.com/en-us/windows/win32/api/lmjoin/nf-lmjoin-netjoindomain
+type DomainJoinOptions uint32
+
+const (
+	// JoinDomainFlag joins the domain specified in lpDomain.
+	JoinDomainFlag DomainJoinOptions = 0x00000001
+	// AcctCreate indicates that the caller will create the account on the domain.
+	AcctCreate DomainJoinOptions = 0x00000002
+	// AcctDelete indicates that lpAccount specifies an account to be deleted during an unjoin operation.
+	AcctDelete DomainJoinOptions = 0x00000004
+	// Win9XUpgrade indicates the join is part of a Win9x upgrade.
+	Win9XUpgrade DomainJoinOptions = 0x00000010
+	// DomainJoinIfJoined causes NetJoinDomain to function even if the workstation is already joined to lpDomain.
+	DomainJoinIfJoined DomainJoinOptions = 0x00000020
+	// JoinUnsecure performs an unsecure join.
+	JoinUnsecure DomainJoinOptions = 0x00000040
+	// MachinePwdPassed indicates that the machine password is being passed.
+	MachinePwdPassed DomainJoinOptions = 0x00000080
+	// DeferSpnSet defers setting the SPN.
+	DeferSpnSet DomainJoinOptions = 0x00000100
+	// JoinDCAccount performs a join as a DC account.
+	JoinDCAccount DomainJoinOptions = 0x00000200
+	// JoinWithNewName performs a join with a new name.
+	JoinWithNewName DomainJoinOptions = 0x00000400
+	// InstallInvocation indicates an install invocation.
+	InstallInvocation DomainJoinOptions = 0x00001000
+	// IgnoreUnsupportedFlags ignores unsupported flags.
+	IgnoreUnsupportedFlags DomainJoinOptions = 0x10000000
+)
+
+var (
+	modnetapi         = windows.NewLazySystemDLL("netapi32.dll")
+	prodNetJoinDomain = modnetapi.NewProc("NetJoinDomain")
+	netJoinDomain     = prodNetJoinDomain.Call
+)
+
+// Domain joins a client to a domain.
+func Domain(domain, joinOU, joinAccount, joinPassword string, options DomainJoinOptions) error {
+
+	dom, err := windows.UTF16PtrFromString(domain)
+	if err != nil {
+		return err
+	}
+	acc, err := windows.UTF16PtrFromString(joinAccount)
+	if err != nil {
+		return err
+	}
+	ou, err := windows.UTF16PtrFromString(joinOU)
+	if err != nil {
+		return err
+	}
+	pw, err := windows.UTF16PtrFromString(joinPassword)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Attempting domain join with domain: %s, OU: %s, account: %s\n", domain, joinOU, joinAccount)
+	if returnCode, _, _ := netJoinDomain(
+		server,                       // lpServer
+		uintptr(unsafe.Pointer(dom)), // lpDomain
+		uintptr(unsafe.Pointer(ou)),  // lpMachineAccountOU
+		uintptr(unsafe.Pointer(acc)), // lpAccount
+		uintptr(unsafe.Pointer(pw)),  // lpPassword
+		uintptr(options),             // fJoinOptions
+	); returnCode != nErrSuccess {
+		return fmt.Errorf("failed to join domain: %w", windows.Errno(returnCode))
+	}
+
+	fmt.Println("Domain join successful.")
+	return nil
+}
---
+++ go/domain/join_test.go
@@ -0,0 +1,113 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package join
+
+import (
+	"errors"
+	"runtime"
+	"syscall"
+	"testing"
+
+	"golang.org/x/sys/windows"
+)
+
+type joinMock struct {
+	retCode uintptr
+	err     error
+}
+
+func (m *joinMock) mockNetJoinDomain(a ...uintptr) (uintptr, uintptr, error) {
+	return m.retCode, 0, m.err
+}
+
+func TestDomain(t *testing.T) {
+	defer func() { netJoinDomain = prodNetJoinDomain.Call }()
+	tests := []struct {
+		name    string
+		retCode uintptr
+		err     error
+		wantErr bool
+	}{
+		{
+			name:    "success",
+			retCode: nErrSuccess,
+		},
+		{
+			name:    "failure",
+			retCode: 1355, // ERROR_NO_SUCH_DOMAIN
+			err:     syscall.Errno(1355),
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := joinMock{retCode: tt.retCode, err: tt.err}
+			netJoinDomain = m.mockNetJoinDomain
+			err := Domain("domain", "ou", "account", "password", DomainJoinOptions(JoinDomainFlag))
+			if !tt.wantErr && err != nil {
+				t.Errorf("Domain() returned unexpected error: %v", err)
+			}
+			if tt.wantErr && err == nil {
+				t.Errorf("Domain() returned success, want error")
+			}
+		})
+	}
+}
+
+func TestDomainLive(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		t.Skip("Skipping live domain join test on non-Windows OS")
+	}
+	// Ensure we are using the real Windows API for this test.
+	netJoinDomain = prodNetJoinDomain.Call
+	t.Log("Attempting live domain join against Windows API; this requires elevation and will fail with dummy data.")
+
+	tests := []struct {
+		name        string
+		domain      string
+		ou          string
+		account     string
+		pass        string
+		wantErr     bool
+		wantErrCode windows.Errno
+	}{
+		{
+			name:        "Live API call with dummy data",
+			domain:      "non-existent-domain-12345",
+			ou:          "OU=Computers",
+			account:     "noaccount",
+			pass:        "nopass",
+			wantErr:     true, // Expect failure
+			wantErrCode: 1355, // ERROR_NO_SUCH_DOMAIN
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Domain(tt.domain, tt.ou, tt.account, tt.pass, DomainJoinOptions(JoinDomainFlag))
+			if !tt.wantErr && err != nil {
+				t.Errorf("Domain() returned unexpected error: %v", err)
+			}
+			if tt.wantErr && err == nil {
+				t.Errorf("Domain() returned success, want error")
+			}
+			if tt.wantErr && err != nil && !errors.Is(err, tt.wantErrCode) {
+				t.Errorf("Domain() returned error %v, want error code %d", err, tt.wantErrCode)
+			}
+			t.Logf("Domain(%q, %q, %q, ...) returned: %v", tt.domain, tt.ou, tt.account, err)
+		})
+	}
+}
